4cfb79776808a7ca274e5b707c5925429f34c3c2
cph-ai-lcw <a0960636350@gmail.com>
2026-07-20 12:30:16 +0800

    v3.0.4：更正領隊與導遊資訊

--- data/trip/leader-config.js
+++ data/trip/leader-config.js
@@ -1,5 +1,6 @@
 export const LEADER_CONFIG = Object.freeze({
-  name: "簡政義 Jason",
+  name: "簡政義",
+  guideName: "JASON",
   phone: "0988825256",
   phoneDisplay: "0988-825-256",
   line: "https://line.me/ti/g/p6dKbt-cU8",
